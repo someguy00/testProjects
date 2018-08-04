bfb82483dd9a551eb93ae52093a03a60551a318c
Gokul V S <ggoku172@gmail.com>
2018-08-04 10:42:17 +0530

    My labs on 4th Aug

---
+++ java/src/Dog.java
@@ -0,0 +1,13 @@
+class Dog {
+    int size;
+    String name;
+    void bark() {
+        if (size > 60) {
+            System.out.println("Wooof! Wooof!");
+        } else if (size > 14) {
+            System.out.println("Ruff! Ruff!");
+        } else {
+            System.out.println("Yip! Yip!");
+        }
+    }
+}
